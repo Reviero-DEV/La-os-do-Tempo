487701d8df7c64272f6da027530b4fe8a78a1f75
Reviero-DEV <netoreviero@gmail.com>
2026-05-02 22:51:11 -0300

    feat(about): adiciona imagem a secao sobre nos

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -1,4 +1,10 @@
-import { FaArrowRight, FaHandHoldingHeart, FaHeart, FaUsers } from "react-icons/fa";
+import {
+  FaArrowRight,
+  FaHandHoldingHeart,
+  FaHeart,
+  FaUsers,
+} from "react-icons/fa";
+import imgAbout from "../assets/images/about/banner-sobrenos.webp";
 
 export default function About() {
   return (
@@ -17,14 +23,17 @@ export default function About() {
           Nosso trabalho é feito por pessoas que acreditam no poder da empatia e
           da solidariedade para transformar vidas.
         </p>
-        <a href="">
+        <a href="/sobre">
           Conheça nossa história <FaArrowRight />
         </a>
+      </div>
+      <div className="about-image">
+        <img src={imgAbout} alt="Casal de idosos sorrindo" />
       </div>
       <div className="tabs">
         <div className="tab-about">
           <div className="icon-about">
-            <FaHandHoldingHeart /> 
+            <FaHandHoldingHeart />
           </div>
           <p>+15 anos de dedicação</p>
         </div>
